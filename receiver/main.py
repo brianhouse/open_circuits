0947d939e46a30372895291388d67a79d1a83122
Brian House <23200+brianhouse@users.noreply.github.com>
2023-12-11 15:38:21 -0500

    receiver

--- receiver/main.py
+++ receiver/main.py
@@ -1,6 +1,6 @@
 #!venv/bin/python
 
-import socket
+import socket, time
 from midi import *
 
 UDP_PORT = 5005
@@ -8,11 +8,19 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", UDP_PORT))
 
+def clamp(value):
+    return min(max(0, int(value)), 127)    
+
 while True:
     try:
+        
         data, addr = sock.recvfrom(1024)
-        message = data.decode('utf-8')
-        print(f"Received message ({message}) from {addr}")
+        cc, value = eval(data.decode('utf-8'))
+        cc = clamp(cc)
+        value = clamp(value)
+
+        channel = 1
+        midi_out.send_control(channel, cc, value)
+
     except KeyboardInterrupt:
         exit()
-
